57af21fe94bfe274d03646a6acad0f76f8b84544
Royserg <staniojakub@gmail.com>
2018-08-14 20:59:44 +0200

    Send translate info to content script
    Needed to merge with langList object to have easy access to language
    codes. API request is done on the event script and translation Sendt
    to the content script

--- src/event/event.js
+++ src/event/event.js
@@ -6,8 +6,34 @@ const menuItem = {
 
 chrome.contextMenus.create(menuItem);
 
-chrome.contextMenus.onClicked.addListener( function(clickData) {
-  if(clickData.menuItemId === 'transl8' && clickData.selectionText) {
-    console.log(clickData.selectionText);
+chrome.contextMenus.onClicked.addListener(translateSelected);
+
+function translateSelected(info, tab) {
+  if (info.menuItemId === 'transl8' && info.selectionText) {
+
+    // retrieve output lang from storage
+    chrome.storage.sync.get(['outputLanguage'], function(data) {
+      // get output language code
+      const outputLang =  langList[data.outputLanguage];
+      // prepare selection for API request
+      const toTranslate = info.selectionText.replace(/\s/g, '+');
+
+      const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180806T180405Z.a97df2323f4db674.6f2961e102f1e464ad056fafcaf6951b74a2fb26&text=${toTranslate}&lang=${outputLang}`;
+
+      fetch(url)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          // send msg to content script in opened tab
+          const msg = {
+            txt: info.selectionText,
+            translation: myJson.text[0]
+          };
+          chrome.tabs.sendMessage(tab.id, msg);
+        })
+
+    });
   }
-})
+}
+
